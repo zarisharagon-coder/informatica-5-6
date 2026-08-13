387ed24ab6e3874aaa800847a45502438ae1f27b
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-13 20:13:42 +0000

    oniko

--- story.py
+++ story.py
@@ -1,18 +1,27 @@
 def main():
-    planet= input("Planet: ")
-    # Separation
-    print("Hello", planet)
+#     planet= input("Planet: ")
+#     # Separation
+#     print("Hello", planet,)
 
 
-    #Ending
-    print("Hello", end= " ")
-    print (planet)
+#     #Ending
+#     print("Hello", end= " ")
+#     print (planet)
 
-    # Concatemation
-    print("Hello " + planet)
+#     # Concatemation
+#     print("Hello " + planet)
 
-    #Formatig String
-    print(f"Hello {planet}")
+#     #Formatig String
+#     print(f"Hello {planet}")
+
+    name= input("What is your name? ")
+    color= input("Tell me a color: ")
+    adj= input("Tell me an adjetive ")
+    goal= input("A goal you would like to achieve")
+
+    print("Hello", name)
+    print("This is your story")
+    print(f"At dawn the sky turned {color} , and the air felt {adj}. I decided today I will finally learn to {goal}")
 
 if __name__ =="__main__":
     main()

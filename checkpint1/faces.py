b0179ac72305443a073808c223b146218ccd317f
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-14 17:20:09 +0000

    checkpint

---
+++ checkpint1/faces.py
@@ -0,0 +1,7 @@
+def main():
+    message=input("Type a message")
+    print(message.replace(":)","🙂").replace(":(","🙁"))
+
+if __name__ =="__main__":
+    main()
+
--- checkpint1/hello.py
+++ checkpint1/hello.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello, world!")
-
+        print("Hello Wolrd")
 if __name__ =="__main__":
     main()
+    

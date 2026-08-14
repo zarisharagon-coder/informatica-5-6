4b432c348d7bd8486f8af16d03273a13f2258a56
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-14 16:58:36 +0000

    Onike

--- story.py
+++ story.py
@@ -14,10 +14,18 @@ def main():
 #     #Formatig String
 #     print(f"Hello {planet}")
 
-    name= input("What is your name? ")
-    color= input("Tell me a color: ")
-    adj= input("Tell me an adjetive ")
-    goal= input("A goal you would like to achieve")
+    name= input("What is your name? ").title().strip()
+    color= input("Tell me a color: ").lower().strip()
+    adj= input("Tell me an adjetive ").lower().strip()
+    goal= input("A goal you would like to achieve").lower().strip()
+
+    print("Hello", name)
+    print("This is your story")
+    print(f"At dawn the sky turned {color} , and the air felt {adj}. I decided today I will finally learn to {goal}")
+    name= input("What is your name? ").title().strip()
+    color= input("Tell me a color: ").lower().strip()
+    adj= input("Tell me an adjetive ").lower().strip()
+    goal= input("A goal you would like to achieve").lower().strip()
 
     print("Hello", name)
     print("This is your story")

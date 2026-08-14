89f5f9304e6247f8d2c26cc6319bd04e9b339c84
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-14 17:15:55 +0000

    checkpointfridayiwannagohome

--- story.py
+++ story.py
@@ -22,14 +22,7 @@ def main():
     print("Hello", name)
     print("This is your story")
     print(f"At dawn the sky turned {color} , and the air felt {adj}. I decided today I will finally learn to {goal}")
-    name= input("What is your name? ").title().strip()
-    color= input("Tell me a color: ").lower().strip()
-    adj= input("Tell me an adjetive ").lower().strip()
-    goal= input("A goal you would like to achieve").lower().strip()
-
-    print("Hello", name)
-    print("This is your story")
-    print(f"At dawn the sky turned {color} , and the air felt {adj}. I decided today I will finally learn to {goal}")
+    print(f"At dawn the sky turned {color} , and the air felt {adj}. I decided today I will finally learn to {goal}.".upper())
 
 if __name__ =="__main__":
     main()

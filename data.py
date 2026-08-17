8326f97a9103d7312157726340998d2ac1007d08
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-17 19:39:27 +0000

    onika

---
+++ data.py
@@ -0,0 +1,12 @@
+def main():
+    width = int(input("Enter the width of the rectangle: "))
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    p = (5*2)+ (width*2)
+    print("perimeter:", p)
+    
+if __name__ =="__main__":
+    main()

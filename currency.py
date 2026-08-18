c09ead1b7f6673fcd6143a3f31f9708a87ee8b8a
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-18 16:35:26 +0000

    onika

--- currency.py
+++ currency.py
@@ -5,7 +5,7 @@ def main():
     d=  (((pesos*0.0054)+(soles*5.07)+(reais*3.28))/17)
     print("Dollars:", d)
     pmx= (d* 17.06)
-    print( "Pesos Mx", pmx)
+    print( "Pesos Mx:", pmx)
 if __name__ =="__main__":
     main()
 

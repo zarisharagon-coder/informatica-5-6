11d9a4607ab1874879f9d3c1e2f3d4783f24afc0
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-18 16:33:23 +0000

    onika

--- data.py
+++ data.py
@@ -7,6 +7,9 @@ def main():
     print("0" * width)
     p = (5*2)+ (width*2)
     print("perimeter:", p)
-    
+    a = (width*5)
+    print("area:", a)
+    diagonal = ((5**2)+(width**2))**0.5
+    print("diagonal", diagonal)
 if __name__ =="__main__":
     main()

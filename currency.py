370375c73043ad877ad858972fe43bfaef17b6f9
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-20 16:40:26 +0000

    onika

--- currency.py
+++ currency.py
@@ -2,9 +2,9 @@ def main():
     pesos = int(input("How much do u have left in pesos??"))
     soles = int(input("How much do u have left in soles??"))
     reais= int(input("How much do u have left in reais??"))
-    d=  (((pesos*0.0054)+(soles*5.07)+(reais*3.28))/17)
+    d=  round(((pesos*0.0054)+(soles*5.07)+(reais*3.28))/17,2)
     print("Dollars:", d)
-    pmx= (d* 17.06)
+    pmx= round(d* 17.06,2)
     print( "Pesos Mx:", pmx)
 if __name__ =="__main__":
     main()
---
+++ update-variable.py
@@ -0,0 +1,39 @@
+def main():
+#Augmented Assigment Operator
+    money = 10
+    money = money + 5
+    print(money) #this will print 15
+
+    #Sybtaction Assignment Operator
+    minutes =60
+    minutes -=25
+    print (minutes) # This will print 35
+
+
+    #Multiply Assigment Operator
+    skill = 10
+    skill *= 2
+    print(skill)
+
+
+
+    text = "Im Enrique"
+    text *=4
+    print(text)
+
+    #Division Assigment Operator
+    pizzas = 8
+    people = int(input("Number of people at the pizza party: "))
+    pizzas /= people
+    print(pizzas)
+
+    #Modulus Assigment Operator
+    leftover = 10
+    leftover %= people
+    leftover %= people
+    print(leftover)
+
+
+
+if __name__ =="__main__":
+    main()

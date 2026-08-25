9546f3703b3bb270da53b4e9a9da6f867ac326b9
zarisharagon-coder <zarisharagon@gmail.com>
2026-08-25 16:37:44 +0000

    onika

---
+++ checkpoint3/rating.py
@@ -0,0 +1,17 @@
+def main():
+    print("EZ burgers")
+    r=float(input("Give us ur honest review,"))
+    if r >= 4.5:
+        print("Yeah, thought so")
+    elif r >= 4:
+        print("good enough")
+    elif r >= 3:
+            print("Good ig 🙄")
+    elif r >= 2:
+            print("Wtv 🫩")
+    else:
+            print("C´mon man")
+
+
+if __name__ =="__main__":
+    main()

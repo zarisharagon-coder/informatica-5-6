426992562685a4c9e84e3e7f96519436ce215095
zarisharagon-coder <zarisharagon@gmail.com>
2026-09-04 17:30:09 +0000

    onika

--- learningapp.py
+++ learningapp.py
@@ -6,16 +6,19 @@ def main():
     print("Hello,",name, " This is EZ math, an easy math learning app,choose how many addittion problems u wanna solve 3-6.")
     dif=int(input("3-6:"))
     if dif == 3:
-        cpu = random.randint(10,99)
-        cpu2 = random.randint(10,99)
+        correct = 0
         print("Okay so u chose to be simple here is the problem")
-    while attempts > 0:
-        print(f"This is the problem {cpu} + {cpu2}")
-        answer = int(input("Whats the answer? "))
-    if answer == (cpu + cpu2):
-        print("smarty pants")
-        attempts -= 1
-        print("Attempts left:", attempts)
+        while correct < 3:
+            cpu = random.randint(10,99)
+            cpu2 = random.randint(10,99)
+            print(f"This is the problem {cpu} + {cpu2}")
+            answer = int(input("Whats the answer? "))
+            if answer == (cpu + cpu2):
+                correct += 1
+                print("smarty pants")
+                print(f"Correct answers::",{correct})
+            else:
+                print("Wrong, try again")
 
 
 

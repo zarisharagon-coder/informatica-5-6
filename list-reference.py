1e140777f382c23d5d51fccff4974d7a4b6b988a
zarisharagon-coder <zarisharagon@gmail.com>
2026-09-10 20:20:26 +0000

    onika

---
+++ list-reference.py
@@ -0,0 +1,10 @@
+def main():
+
+    fruits=["Apple","Orange","Grapes"]
+    fruits.append("Banana")
+    print(fruits)
+    fruits1= ["Apple","Orange","Grapes"]
+    fruits1.insert(2, "banana")
+    print(fruits1)
+if __name__ =="__main__":
+    main()
